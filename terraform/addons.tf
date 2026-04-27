a2e4cb242c0405e5ba55d2384ca9b86f8098d833
deepaksoam313 <deepaksoam313@gmail.com>
2026-04-27 21:02:02 +0530

    Argo - added loki

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -100,10 +100,31 @@ module "eks_addons" {
 #   =============================================================================
 #   OPTIONAL: AWS LOAD BALANCER CONTROLLER
 #   =============================================================================
+#   enable_aws_load_balancer_controller = true
+#   aws_load_balancer_controller = {
+#     most_recent = true
+#     namespace   = "kube-system"
+#   }
+
   enable_aws_load_balancer_controller = true
   aws_load_balancer_controller = {
     most_recent = true
     namespace   = "kube-system"
+    # ADD THIS PART:
+    set = [
+      {
+        name  = "vpcId"
+        value = module.vpc.vpc_id
+      },
+      {
+        name  = "region"
+        value = var.aws_region
+      },
+      {
+        name  = "clusterName"
+        value = module.retail_app_eks.cluster_name
+      }
+    ]
   }
 
   depends_on = [module.retail_app_eks]
